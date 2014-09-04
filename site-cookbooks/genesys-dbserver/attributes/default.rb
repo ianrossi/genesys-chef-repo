416b3107bb525db571dd0ba368399fe818028dd8
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-09-04 11:32:56 -0400

    First working default recipe

--- site-cookbooks/genesys-dbserver/attributes/default.rb
+++ site-cookbooks/genesys-dbserver/attributes/default.rb
@@ -1,7 +1,16 @@
-default[:genesys_dbserver][:package] = {
-  :rpm_name => "DBServer-ENU",
-  :version => "8.1.300.05",
-  :build_number => 1
+default[:genesys_dbserver] = {
+  :package => {
+    :repository_name => "genesys_repo",
+    :repo_base_url => "http://192.168.2.105/yum/x86_64",
+    :rpm_name => "DBServer-ENU",
+    :version => "8.1.300.05",
+    :build_number => 1,
+  },
+  :service_name => "genesys-dbserver",
+  :user => "genesys",
+  :group => "genesys",
+  :kill_wait_time => 10
 }
 
 default[:genesys][:home] = "/usr/local/genesys"
+
